0ceed5a69159b5274298938154a3bf8e5a7d3024
hillmanation <jake.hillman.it@gmail.com>
2024-09-20 00:20:28 -0400

    Update bnu-discord-bot.py

--- src/bnu-discord-bot.py
+++ src/bnu-discord-bot.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/app')  # Ensure /app is added to the Python path
 from api.discord_bot.bnu_api import bot  # Import the bot instance from bnu_api
 from api.discord_bot.bot_config import bot_token
 
