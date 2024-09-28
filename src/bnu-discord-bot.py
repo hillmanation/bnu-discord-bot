85a7a8b03b018c47693017d66090097eaf551e1f
hillmanation <jake.hillman.it@gmail.com>
2024-09-28 13:48:08 -0400

    Update sys path

--- src/bnu-discord-bot.py
+++ src/bnu-discord-bot.py
@@ -1,5 +1,8 @@
 import sys
+import os
 #sys.path.append('/app')  # Ensure /app is added to the Python path
+# Add the parent directory of src to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from api.discord_bot.bnu_api import bot  # Import the bot instance from bnu_api
 from api.discord_bot.bot_config import bot_token
 import utilities.logging_config as logging_config
