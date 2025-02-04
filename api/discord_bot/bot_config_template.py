a76fcea191221d4b26a20c3677de2aafb04fa329
hillmanation <jake.hillman.it@gmail.com>
2025-02-04 18:49:23 -0500

    Remove deprecated file and add config templates

---
+++ api/discord_bot/bot_config_template.py
@@ -0,0 +1,10 @@
+bot_token = ''
+guild_id = ''
+default_message_channel = ''
+test_message_channel = ''
+
+'''
+Rename this file to 'bot_config.py' and fill in the above information
+You can find the information you need to create/setup your bot here:
+https://discordpy.readthedocs.io/en/stable/discord.html
+'''
---
+++ api/kavita_query/kavita_config_template.py
@@ -0,0 +1,12 @@
+kavita_base_url=""
+opds_url=''
+kavi_api_key=''
+server_admin_id=''  # Discord ID of the Discord server admin
+
+'''
+Rename this file to 'kavita_config.py' and fill in the above information
+Information on getting the opds and kavita api key from your server can be found here:
+https://wiki.kavitareader.com/guides/api/
+It can also generally be found in your server settings under Account > API Key/OPDS
+i.e. https://your.kavita.server.url/settings#clients
+'''
